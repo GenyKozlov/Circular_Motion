34277d0ab5ff664e19c5937dd58a1fe01748127c
GenyKozlov <98642132+GenyKozlov@users.noreply.github.com>
2023-04-29 19:25:24 +0500

    edit 2Dplot

--- graphics/2Dplot.py
+++ graphics/2Dplot.py
@@ -5,7 +5,7 @@ import math
 
 plt.rcParams["figure.autolayout"] = True
 
-df = pd.read_csv(r'data/3 copters with constant centre.csv', header=0, delimiter=';')
+df = pd.read_csv(r'data/Sim_cf/test_high_level_circ3_moving_center_logging1.csv', header=0, delimiter=';')
 
 cols = df.columns
 for col in cols:
@@ -14,11 +14,32 @@ for col in cols:
 print(df.columns)
 
 
-# ax = df.plot(x='i', y=['p12_APF', 'p23'])
-# ax.set(xlabel='Time, [ds]', ylabel='Phase shifts, [rad]')
+ax=df.plot(x='i', y=['pz_1'])
+ax.set(xlabel='Steps', ylabel='z coordinate 1-d copter, [m]')
+
+ax=df.plot(x='i', y=['pz_2'])
+ax.set(xlabel='Steps', ylabel='z coordinate 2-d copter, [m]')
+
+ax=df.plot(x='i', y=['pz_3'])
+ax.set(xlabel='Steps', ylabel='z coordinate 3-d copter, [m]')
+
+ax = df.plot(x='px_1', y=['py_1'])
+ax.set(xlabel='x coordinate 1-d copter, [m]', ylabel='y coordinate 1-d copter, [m]')
+
+ax = df.plot(x='px_2', y=['py_2'])
+ax.set(xlabel='x coordinate 2-d copter, [m]', ylabel='y coordinate 2-d copter, [m]')
+
+ax = df.plot(x='px_3', y=['py_3'])
+ax.set(xlabel='x coordinate 3-d copter, [m]', ylabel='y coordinate 3-d copter, [m]')
+
+ax = df.plot(x='i', y=['p_12','p_23'])
+ax.set(xlabel='Steps', ylabel='Phase shifts, [rad]')
 
 ax = df.plot(x='i', y=['d_1','d_2', 'd_3'])
-ax.set(xlabel='Time, [ds]', ylabel='Distances to the center, [m]')
+ax.set(xlabel='Steps', ylabel='Distances to the center, [m]')
+
+# ax = df.plot(x='i', y=['distance_12'])
+# ax.set(xlabel='Steps', ylabel='Distance between UAVs, [m]')
 
 
 # fig, axes = plt.subplots(nrows=3, ncols=1)
@@ -30,22 +51,5 @@ ax.set(xlabel='Time, [ds]', ylabel='Distances to the center, [m]')
 
 # ax=df.plot(x='px_3', y=['py_3'], ax=axes[2])
 # ax.set(xlabel='px_3, [m]', ylabel='py_3, [m]')
-
-
-ax = df.plot(x='px_1', y=['py_1'])
-ax = df.plot(x='px_2', y=['py_2'])
-ax = df.plot(x='px_3', y=['py_3'])
-
-ax=df.plot(x='i', y=['pz_2'])
-ax.set(xlabel='px_1, [m]', ylabel='py_1, [m]')
-
-ax=df.plot(x='i', y=['pz_3'])
-ax.set(xlabel='px_2, [m]', ylabel='py_2, [m]')
-
-# df.plot(x='i', y=['setPx1','setPx2'])
-# df.plot(x='i', y=['setPy1','setPy2'])
-
-# ax = df.plot(x='i', y=['d_1','d_2'])
-# ax.set(xlabel='Time, [ds]', ylabel='Distances to the center, [m]')
 
 plt.show()
